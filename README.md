45ecddae94bc786d1b0a48d0e4c5cf24ccb116a3
piyushs11 <88674567+piyushs11@users.noreply.github.com>
2022-11-05 20:18:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Portfolio
+# React Portfolio Project
+
+- npx create react app
+
+- npm
+  - tailwind
+  - react icons
+- get custom fonts
+
+  - great vibes
+  - raleway
+  - add great vibes to tailwind config
+
+- add assets
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    fontFamily: {
+      signature: ["Great Vibes"],
+    },
+  },
+  plugins: [],
+};
